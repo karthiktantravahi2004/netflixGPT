20e07ee9d0912a8139a01c2f91867c1648855b99
karthik <22p31a05c6@acet.ac.in>
2025-11-06 22:21:23 +0530

    06-11-2025

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -6,12 +6,12 @@ const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
   if (!movies) return;
   const mainMovies = movies[0];
-  console.log(mainMovies);
-  const { original_title, overview } = mainMovies;
+  //console.log(mainMovies);
+  const { original_title, overview, id } = mainMovies;
   return (
     <div>
       <VideoTitle title={original_title} overview={overview} />
-      <VideoBackground />
+      <VideoBackground movieId={id} />
     </div>
   );
 };
