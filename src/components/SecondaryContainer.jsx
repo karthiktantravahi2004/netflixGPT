23bf58acfaad59c1b10ce7f5a31b06a9ea7532b9
karthik <22p31a05c6@acet.ac.in>
2025-11-10 23:24:43 +0530

    10-11-2025

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -11,10 +11,6 @@ const SecondaryContainer = () => {
         <div className="-mt-52 pl-12 relative z-20">
           <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
           <MovieList title="Popular" movies={movies.popularMovies} />
-          <MovieList title="Popular" movies={movies.popularMovies} />
-          <MovieList title="Popular" movies={movies.popularMovies} />
-          <MovieList title="Popular" movies={movies.popularMovies} />
-          <MovieList title="Popular" movies={movies.popularMovies} />
         </div>
       </div>
     )
